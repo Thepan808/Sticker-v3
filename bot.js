5cf16e0cf2af17f807a147675d04cb71e8eda9b6
The Vampiro Elegant <streetdemonic003@gmail.com>
2022-12-30 06:55:24 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ bot.on(['channel_post', 'edited_channel_post', 'poll'], () => {})
 const { match } = I18n
 const i18n = new I18n({
   directory: path.resolve(__dirname, 'locales'),
-  defaultLanguage: 'en',
+  defaultLanguage: 'pt_BR',
   defaultLanguageOnMissing: true
 })
 
